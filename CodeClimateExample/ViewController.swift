fe6925e43607b5d2b08f9fe7a376471ce5d43910
Gerhard Koekemoer <gerhard@glucode.com>
2019-03-04 14:15:15 +0200

    Init repo

---
+++ CodeClimateExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeClimateExample
+//
+//  Created by Gerhard Koekemoer on 2019/03/04.
+//  Copyright © 2019 Gerhard Koekemoer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CodeClimateExampleTests/CodeClimateExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  CodeClimateExampleTests.swift
+//  CodeClimateExampleTests
+//
+//  Created by Gerhard Koekemoer on 2019/03/04.
+//  Copyright © 2019 Gerhard Koekemoer. All rights reserved.
+//
+
+import XCTest
+@testable import CodeClimateExample
+
+class CodeClimateExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
